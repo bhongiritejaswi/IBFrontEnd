fa68e89a3a758d955ab899d939eaf55e63df8477
bhongiritejaswi24 <bhongiritejaswi24@gmail.com>
2019-08-24 04:57:49 -0600

    Commiting Fund Transfer

--- src/app/fund-transfer-sameacc/fund-transfer-sameacc.component.ts
+++ src/app/fund-transfer-sameacc/fund-transfer-sameacc.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ListAccountsService } from '../list-accounts.service';
 import {  Accounts } from '../CustomerAccounts';
 import {Subscription} from 'rxjs';
+import { map , filter} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-fund-transfer-sameacc',
@@ -10,9 +12,10 @@ import {Subscription} from 'rxjs';
 })
 export class FundTransferSameaccComponent implements OnInit {
 
-  accounts:Accounts[]
+  accounts:Accounts[];
+  dropDownAccounts:Accounts[];
   private subscription:Subscription;
-
+  from;
   constructor(private listaccountsservice:ListAccountsService) { }
 
   ngOnInit() {
@@ -22,7 +25,21 @@ export class FundTransferSameaccComponent implements OnInit {
                               .subscribe(response => {
                                 this.accounts = response;
                               })
+    this.dropDownAccounts=this.accounts;
 
   }
+
+  fromSelected(from){
+
+    this.dropDownAccounts=this.accounts;
+    this.dropDownAccounts = this.dropDownAccounts.filter(function(res){
+       if (res.acnumber != from ){
+        // console.log("Hii");
+         return res;
+       }
+    });
+    console.log(this.dropDownAccounts);
+  }
+  
   
 }
